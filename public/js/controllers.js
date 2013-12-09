f628734a037c382740974e44c30c7316e08b7089
Steve Bering <stevebering@gmail.com>
2013-12-09 10:07:04 -0800

    work on adding users

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -24,21 +24,22 @@ function AddSessionController($scope, $http, $location) {
     $scope.submitSession = function() {
         $http.post('/api/sessions', $scope.form)
             .success(function(data) {
-                $location.path('/');
+                $location.path('#/sessions');
             });
     };
 }
 
 function AddUserController($scope, $http, $location) {
-    var user = $scope.user;
-    console.log('Posting ' + JSON.stringify(user));
     $scope.addUser = function() {
+        var user = $scope.user;
+        console.log('Posting ' + JSON.stringify(user));
         $http.post('/api/users', user)
             .success(function(data) {
                 $location.path('/users');
+                console.log($location.path());
             })
             .error(function() {
-                console.log(err);
+                console.log('Unable to add user.');
             });
     };
 }
